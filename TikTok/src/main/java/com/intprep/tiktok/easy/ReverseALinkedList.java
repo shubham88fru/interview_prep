ce8469b6a791408edf8ebc489160a4b3ff214fb2
shubham88fru <shubham88fru@yahoo.com>
2024-08-27 06:30:37 -0700

    reverse a linked list

---
+++ TikTok/src/main/java/com/intprep/tiktok/easy/ReverseALinkedList.java
@@ -0,0 +1,29 @@
+package com.intprep.tiktok.easy;
+
+public class ReverseALinkedList {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) return null;
+
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = head.next;
+
+        while (curr != null) {
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+
+            if (next != null) next = next.next;
+        }
+
+        return prev;
+    }
+}
+
+class ListNode {
+  int val;
+  ListNode next;
+  ListNode() {}
+  ListNode(int val) { this.val = val; }
+  ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
