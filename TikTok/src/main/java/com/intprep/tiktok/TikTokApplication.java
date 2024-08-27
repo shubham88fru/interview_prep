f062c25ffcbd186b5ceacf373d0fb639701e2d0f
shubham88fru <shubham88fru@yahoo.com>
2024-08-27 06:08:51 -0700

    Initial commit

---
+++ TikTok/src/main/java/com/intprep/tiktok/TikTokApplication.java
@@ -0,0 +1,13 @@
+package com.intprep.tiktok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikTokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TikTokApplication.class, args);
+    }
+
+}
---
+++ TikTok/src/test/java/com/intprep/tiktok/TikTokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intprep.tiktok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TikTokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
