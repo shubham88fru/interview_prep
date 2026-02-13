836d148faaeb6a7b5382bd3f596e79c8e23a3da8
shubham88fru <shubham88fru@yahoo.com>
2026-02-12 20:39:00 -0600

    add more examples

--- src/main/java/JavaStreamsInterviewQuestions.java
+++ src/main/java/JavaStreamsInterviewQuestions.java
@@ -1,10 +1,13 @@
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class JavaStreamsInterviewQuestions {
     public static void main(String[] args) {
-        List<Integer> ints = List.of(5, 1, 2, 10, 11, 1, 3);
-        List<String> strs = List.of("game", "name", "same", "else", "throw", "trash");
+        List<Integer> ints = List.of(5, 1, 2, 13, 10, 11, 1, 3, 5, 10, 15);
+        List<String> strs = List.of("longstring", "game", "name", "same", "alright", "o", "oai", "else", "throw", "trash");
 
         /*
             1. Write a program to find the sum of all elements
@@ -96,5 +99,108 @@ public class JavaStreamsInterviewQuestions {
              in a list of integers using Java Stream API.
         */
         ints.stream().reduce(1, (a, b) -> a*b);
+
+        /*
+            16. Given a list of integers, write a program to find and
+            print the prime numbers using Java Stream API.
+        */
+        ints.stream().filter(i -> isPrime(i)).toList();
+
+        /*
+            17. Write a program to check if a list of strings contains
+             a specific string using Java Stream API.
+        */
+        strs.stream().anyMatch(s -> s.equals("name"));
+
+        /*
+            18. Given a list of strings, write a program to find and print the
+             strings with length greater than a specified value 5 using Java Stream API.
+        */
+        strs.stream().filter(s -> s.length()>5).toList();
+
+        /*
+            19. Write a program to filter out all the elements divisible
+             by 3 and 5 from a list of integers using Java Stream API.
+        */
+        ints.stream().filter(i -> (i%3==0 && i%5==0)).toList();
+
+        /*
+            20. Given a list of strings, write a program to find and print
+             the strings with the maximum length using Java Stream API.
+        */
+        strs.stream().max((s1, s2) -> s1.length()-s2.length()).orElse(null);
+
+        /*
+            21. Write a program to reverse a list of strings using Java Stream API.
+        */
+        Collections.reverse(strs);
+
+        /*
+            22. Given a list of integers, write a program to find and print
+            the distinct odd numbers using Java Stream API.
+        */
+        ints.stream().filter(i-> i%2!=0).distinct().toList();
+
+        /*
+            23. Write a program to remove all null values from a list
+             of strings using Java Stream API.
+        */
+        strs.stream().filter(s -> s != null).toList();
+
+        /*
+            24. Given a list of integers, write a program to find and
+             print the sum of all odd numbers using Java Stream API.
+        */
+        ints.stream().filter(i -> i%2 != 0).mapToInt(i->i).sum();
+
+        /*
+            25. Write a program to find the intersection of two
+             lists of strings using Java Stream API.
+        */
+        ints.stream().filter(i -> List.of(2, 3, 8, 9, 1).contains(i)).toList();
+
+        /*
+            26. Given a list of strings, write a program to find and print the
+             strings containing only vowels using Java Stream API.
+        */
+        strs.stream().filter(s -> s.matches("[aeiouAEIOU]+")).toList();
+
+        /*
+            27. Write a program to convert a list of strings to a
+             comma-separated string using Java Stream API.
+        */
+        strs.stream().collect(Collectors.joining(", "));
+
+        /*
+            28. Given a list of integers, write a program to find and
+             print the index of the first occurrence of a specific number
+             using Java Stream API.
+        */
+        IntStream.range(0, ints.size()).filter(i -> ints.get(i).equals(13))
+                .findFirst().orElse(-1);
+
+        /*
+            29. Write a program to find the union of two lists
+             of integers using Java Stream API.
+        */
+        Stream.concat(ints.stream(), List.of(4, -1, 2, 0).stream()).distinct().toList();
+
+        /*
+            30. Given a list of strings, write a program to find
+             and print the strings containing duplicate characters
+             using Java Stream API.
+        */
+        strs.stream().filter(s -> s.length() != s.chars().distinct().count()).toList();
+    }
+
+    private static boolean isPrime(int num) {
+        if (num == 1) return false;
+        if (num == 2) return true;
+
+        for (int i=2; i<=Math.sqrt(num); i++) {
+            if (num%i == 0) return false;
+        }
+
+        return true;
     }
 }
