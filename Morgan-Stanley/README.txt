7bd0953c6d154249152d63cb4ae83370cf18e36a
shubham88fru <shubham88fru@yahoo.com>
2026-02-21 15:57:12 -0600

    morgan stanley

---
+++ Morgan-Stanley/README.txt
@@ -0,0 +1,6 @@
+# MS Top LC
+
+22. LC 49. Group anagrams
+    i. brute - sort each string and map
+        a. TC: O(n*klog(k)), SC: O(n), where n is num of string and k is length of max string.
+    a. Hashmap and freq array --> O(n*k)
---
+++ Morgan-Stanley/src/main/java/com/learning/morganstanley/MorganStanleyApplication.java
@@ -0,0 +1,13 @@
+package com.learning.morganstanley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorganStanleyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MorganStanleyApplication.class, args);
+    }
+
+}
