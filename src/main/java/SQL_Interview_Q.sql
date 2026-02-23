31b84f5051f45d1104bad081ab1f5320e94af8cd
shubham88fru <shubham88fru@yahoo.com>
2026-02-22 19:43:19 -0600

    fix typo

--- src/main/java/SQL_Interview_Q.sql
+++ src/main/java/SQL_Interview_Q.sql
@@ -267,7 +267,7 @@ FROM admissions
 SELECT p.patient_id, p.first_name, p.last_name
 FROM patients p
 LEFT JOIN admissions a ON p.patient_id = a.patient_id
-WHERE a.patient_id iS null;
+WHERE a.patient_id IS null;
 
 
 /*
